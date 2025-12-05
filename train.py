04614bbd989ec2e7aea202f8bbfe539f4d53f730
Md Ashraful Islam <md.ashraful.islam.de@gmail.com>
2025-12-05 13:28:21 +0100

    code refactroing, dynamic sizing in CNN dense layer architecture, training code modification

--- train.py
+++ train.py
@@ -1,35 +1,28 @@
 import torch
 from torch.utils.data import DataLoader
-from data_processing import AudioDataset
+from audio_dataset import AudioDataset
 from model_architecture import AudioCNN
 from split_dataset import split_dataset
-from config import BATCH_SIZE, EPOCHS, LEARNING_RATE, MODEL_PATH
+from config import BATCH_SIZE, EPOCHS, LEARNING_RATE, MODEL_PATH, N_FFT, TARGET_SAMPLE_RATE, HOP_LENGTH, N_MELS
 from torch.nn.utils.rnn import pad_sequence
+import torchaudio
+def get_transformation():
+    mel_transform = torchaudio.transforms.MelSpectrogram(
+        sample_rate=TARGET_SAMPLE_RATE,
+        n_fft=N_FFT,
+        hop_length=HOP_LENGTH,
+        n_mels=N_MELS
+    )
+    return mel_transform
 
-def custom_collate_fn(batch):
-    # Reshape: (1, Bands, Time) -> (Time, Bands)
-    mels = [item[0].squeeze(0).transpose(0, 1) for item in batch]
-    labels = [item[1] for item in batch] # Labels are still Python ints here
-
-    # 2. Pad the sequences
-    # Output is (Batch, Max_Time, Bands)
-    padded_mels = pad_sequence(mels, batch_first=True, padding_value=0.0)
-
-    # 3. CONVERT LABELS TO TENSORS AND STACK (THE FIX)
-    # We must convert the list of Python ints into a list of PyTorch Tensors
-    # The labels represent classes, so we use dtype=torch.long
-    labels_stacked = torch.stack([torch.tensor(label, dtype=torch.long) for label in labels])
-
-    # Output shape: (Batch, Max_Time, Bands), Labels: (Batch)
-    return padded_mels, labels_stacked
 
 # Load split datasets
 (train_files, train_labels), (val_files, val_labels), _ = split_dataset()
 
-train_dataset = AudioDataset(train_files, train_labels)
-val_dataset = AudioDataset(val_files, val_labels)
+train_dataset = AudioDataset(train_files, train_labels,get_transformation())
+val_dataset = AudioDataset(val_files, val_labels,get_transformation())
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True,collate_fn=custom_collate_fn)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
 
 model = AudioCNN()
@@ -42,7 +35,7 @@ for epoch in range(EPOCHS):
     running_loss = 0
     correct_train = 0
     for mel, label in train_loader:
-        mel = mel.unsqueeze(1)
+        #mel = mel.unsqueeze(1)
         optimizer.zero_grad()
         pred = model(mel)
         loss = criterion(pred, label)
@@ -58,7 +51,7 @@ for epoch in range(EPOCHS):
     correct_val = 0
     with torch.no_grad():
         for mel, label in val_loader:
-            mel = mel.unsqueeze(1)
+            #mel = mel.unsqueeze(1)
             pred = model(mel)
             correct_val += (pred.argmax(1) == label).sum().item()
     val_acc = correct_val / len(val_dataset)
